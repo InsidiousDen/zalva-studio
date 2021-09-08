2dbc1a1be0b3b2272118f09bc9e75a64470e7d27
InsidiousDen <InsidiousDen>
2021-09-08 09:39:22 +0300

    parallax + scroll

--- script.js
+++ script.js
@@ -1,51 +1,33 @@
-$(document).ready(function(){
-  $(window).on('scroll',function(){
-
-    let link = $('.navbar a.dot');
-    let top = $(window).scrollTop();
-
-    $('.slide').each(function(){
-      let id = $(this).attr('id');
-      let height = $(this).height();
-      let offset = $(this).offset().top - 150;
-      if(top >= offset && top < offset + height){
-        link.removeClass('active');
-        $('.navbar').find('[data-scroll="' + id + '"]').addClass('active');
-      }
-    });
-  });
-});
-
-$(document).ready(function () {
-  let divs = $('.slide');
-  let dir = 'up';
-  let div = 0; 
-  
-  $(document.body).on('DOMMouseScroll mousewheel', function (e) {
-    if (e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) {
-        dir = 'down';
-    } else {
-        dir = 'up';
-    }
-    div = -1;
-    divs.each(function(i){
-        if (div<0 && ($(this).offset().top >= $(window).scrollTop())) {
-            div = i;
-        }
-    });
-    if (dir == 'up' && div > 0) {
-        div--;
-    }
-    if (dir == 'down' && div < divs.length) {
-        div++;
-    }
-      $('html,body').stop().animate({
-        scrollTop: divs.eq(div).offset().top
-    }, 200);
-    return true; 
-  });
-  $(window).resize(function () {
-      $('html,body').scrollTop(divs.eq(div).offset().top);
-  });
-});
+let pageSlider = new Swiper(".page",{
+  wrapperClass: 'page__wrapper',
+  slideClass: 'page__screen',
+  direction: 'vertical',
+  slidesPreView: 'auto',
+  parallax: true,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+  mousewheel: {
+    sensitivity: 1,
+  },
+  watchOverflow: true,
+  speed: 1500,
+  observer: true,
+  observeParents: true,
+  observeSlidesChildren: true,
+  pagination: {
+    el: '.page__pagination',
+    type: 'bullets',
+    clickable: true,
+    bulletClass: 'page__bullet',
+    bulletActiveClass: 'page__bullet__active',
+  },
+  scrollbar: {
+    el: '.page__scroll',
+    dragClass: 'page__drag-scroll',
+    draggable: true,
+  },
+})
 
